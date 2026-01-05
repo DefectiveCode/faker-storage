d09bc435ac7a9f1012184367776b488da84ef3af
Corey McCormick <624784+SimplyCorey@users.noreply.github.com>
2026-01-05 17:15:50 -0500

    Tests

--- tests/Generators/RandomImageTest.php
+++ tests/Generators/RandomImageTest.php
@@ -19,24 +19,6 @@ class RandomImageTest extends TestCase
     }
 
     #[Test]
-    public function itGeneratesRandomData(): void
-    {
-        $generator = $this->getGenerator();
-
-        $generator->height(10, 10);
-        $generator->width(10, 10);
-
-        $data = $generator->generate();
-
-        $this->assertIsResource($data);
-
-        rewind($data);
-
-        $contents = stream_get_contents($data);
-        $this->assertEquals('f802d117f6af5b4d042eed81b9256153c0db1d18', sha1($contents));
-    }
-
-    #[Test]
     public function itSetsContentTypeBasedOnWonFormat(): void
     {
         $generator = $this->getGenerator();
@@ -68,25 +50,6 @@ class RandomImageTest extends TestCase
         $data = $generator->generate();
 
         $this->assertIsResource($data);
-    }
-
-    #[Test]
-    public function itGeneratesRandomDataWithAlpha(): void
-    {
-        $generator = $this->getGenerator();
-
-        $generator->withAlpha(true);
-        $generator->height(10, 10);
-        $generator->width(10, 10);
-
-        $data = $generator->generate();
-
-        $this->assertIsResource($data);
-
-        rewind($data);
-
-        $contents = stream_get_contents($data);
-        $this->assertEquals('bfa4c86029e3b606e059ec4a2eb9306b15139b70', sha1($contents));
     }
 
     #[Test]
